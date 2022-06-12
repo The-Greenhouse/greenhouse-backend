53d0396e12fc9a58e138b6c7ff6414492331cc83
ayushsatyam146 <ayushsatyam146@gmail.com>
2022-06-12 17:19:10 +0530

    added Farm model

--- models/Farm.js
+++ models/Farm.js
@@ -1,7 +1,34 @@
-// Farm {
-//     Broker,
-//     Billing,
-//     Block[],
-//     User,
-//     date,
-// }
+const mongoose = require('mongoose');
+
+const farmSchema = new mongoose.Schema({
+    brokerId: {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: "Broker"
+    },
+    billingId : {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: "Broker"
+    },
+    blockId : [
+        {
+            type: mongoose.Schema.ObjectId,
+            require: true,
+            ref: "farm",
+            default: []
+        }
+    ],
+    userId : {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: "User"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Farm', farmSchema);
+
