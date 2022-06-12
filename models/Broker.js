fb95ec4f31b35c08835965f30bcca7acb999a265
ayushsatyam146 <ayushsatyam146@gmail.com>
2022-06-12 17:39:38 +0530

    added broker model

--- models/Broker.js
+++ models/Broker.js
@@ -1,5 +1,20 @@
-// Broker {
-//     url,
-//     port,
-//     Block[]
-// }
+const mongoose = require('mongoose');
+
+const brokerchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    port: {
+        type: String,
+        required: true
+    },
+    farmId : {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: "Broker"
+    }
+});
+
+module.exports = mongoose.model('Farm', brokerSchema);
+
