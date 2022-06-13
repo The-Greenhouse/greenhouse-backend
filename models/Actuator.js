2ceba5c3222fb4e9bb51b9439b27de67270d6093
ayushsatyam146 <ayushsatyam146@gmail.com>
2022-06-13 11:17:14 +0530

    added Actuator Schema

--- models/Actuator.js
+++ models/Actuator.js
@@ -1,8 +1,33 @@
-// actuator {
-//     type, // in measuring units
-//     value,
-//     block_id,
-//     date_installed,
-//     date_last_maintained,
-//     last_activity     
-// }
+const mongoose = require('mongoose');
+
+const billingSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true,
+        ref: "farm_variable_type"
+    },
+    value : {
+        type: mongoose.Schema.Types.Decimal128,
+        required: true,
+    },
+    blockId : {
+        type: mongoose.Schema.Types.Decimal128,
+        required: true,
+    },
+    dateInstalled : {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateLastMaintained : {
+        type: Date,
+        default: Date.now
+    },
+    lastActivity : {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Billing', billingSchema);
+
