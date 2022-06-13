9f62583dc23a77e3b374df62b4eeadfdc4db8695
ayushsatyam146 <ayushsatyam146@gmail.com>
2022-06-13 11:22:23 +0530

    added block model

--- models/Block.js
+++ models/Block.js
@@ -1,5 +1,30 @@
+const mongoose = require('mongoose');
+
+const billingSchema = new mongoose.Schema({
+    locationTag: {
+        type: String,
+    },
+    sensors: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "Sensor",
+            default: []
+        }
+    ],
+    actuators: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "Actuator",
+            default: []
+        }
+    ]
+});
+
+module.exports = mongoose.model('Billing', billingSchema);
+
 // Block {
 //     location_Number,
 //     Sensor[],
 //     Actuators[],
 // }
+
